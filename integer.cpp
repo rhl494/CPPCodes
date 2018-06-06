daed27b5224675d1db03ab4a18038f636b17c449
Rahul Saxena <rahulsaxena494@yahoo.com>
2018-06-06 15:51:10 -0400

    Create integer.cpp

---
+++ integer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int x;
+
+	int y;
+
+	cout << "Type in x" << endl;
+	cin >> x;
+	
+	cout << "Type in y" << endl;
+	cin >> y;
+
+	int sum = x + y;
+
+	cout << "Total is " << sum << endl;
+
+	return 0;
+
+}
